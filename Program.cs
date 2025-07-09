578fd0d27dfb476184e8d74b6362769cdaa57c58
kavya <kavyamaurya269@gmail.com>
2025-07-09 14:08:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Program 
+{
+class Program
+{
+    static void Main(string[]args)
+    {
+        Console.Write("to calculate sum of first N natural number; please provide value for 'N' ---> ");
+        int N = Convert.ToInt32(Console.ReadLine());
+         
+         int sum = N*(N+1)/2 ;
+
+         Console.WriteLine($"{sum}");
+         
+
+    }
+}
+}       
