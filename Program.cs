b2352debf31edac90e164c63bec4bf58a1eaa88c
kavya <kavyamaurya269@gmail.com>
2025-07-09 14:09:37 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,9 +5,10 @@ class Program
 {
     static void Main(string[]args)
     {
-        Console.Write("to calculate sum of first N natural number; please provide value for 'N' ---> ");
+        Console.Write("to calculate sum of first N natural number; please provide value for 'N' here ---> ");
         int N = Convert.ToInt32(Console.ReadLine());
-         
+
+        
          int sum = N*(N+1)/2 ;
 
          Console.WriteLine($"{sum}");
